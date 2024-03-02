9fa412ecb2744698be0201a19c6273b7beb4357c
cbh123 <charlieholtz@gmail.com>
2024-03-02 10:25:45 -0800

    remove init livereact

--- assets/js/app.js
+++ assets/js/app.js
@@ -80,10 +80,6 @@ let liveSocket = new LiveSocket("/live", Socket, {
   hooks: Hooks,
 });
 
-document.addEventListener("DOMContentLoaded", (e) => {
-  initLiveReact();
-});
-
 // Show progress bar on live navigation and form submits
 topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
 window.addEventListener("phx:page-loading-start", (info) => topbar.show());
