7b75e437c70d7fed37c08bc854e2e5584d0d6ff1
cbh123 <charlieholtz@gmail.com>
2024-03-04 11:45:48 -0700

    ahh wrong endpoint

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 
-config :toe, StickerWeb.Endpoint,
+config :toe, ToeWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "bidtactoe.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
